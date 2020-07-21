e73727ebc84f5467ca3315609560234ebe84f4c5
Stuart Axon <stuaxo2@yahoo.com>
2020-07-21 12:10:16 +0100

    Update setup.py
    Bump pytest version to 4.6 as required by pytest-cov.

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     include_package_data=True,
     zip_safe=True,
-    extras_require={"test": ["pytest>=3.0", "pytest-cov", "pydocstyle",
+    extras_require={"test": ["pytest>=4.6", "pytest-cov", "pydocstyle",
                              "flake8", "coveralls"]},
 )
