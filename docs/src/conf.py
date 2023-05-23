d790bdd6e2044bf76f7309cca6c63814f186e3e7
Sean Gillies <sean.gillies@gmail.com>
2023-05-23 14:44:46 -0600

    Rotzbua add doc gen (#99)
    * Add documentation generation by `sphinx`
    
    Automatically build and deployed by github actions to github pages.
    
    * Switch to RTD
    
    * Fix config path
    
    * Add badge
    
    ---------
    
    Co-authored-by: Rotzbua <Rotzbua@users.noreply.github.com>

---
+++ docs/src/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+
+import sys
+
+sys.path.append(os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'rasterio/affine'
+copyright = '2023, Sean Gillies'
+author = 'Sean Gillies'
+release = 'development'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['Thumbs.db', '.DS_Store']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The root toctree document.
+root_doc = 'index'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+
+html_static_path = ['_static']
+
+# If this is not None, a ‘Last updated on:’ timestamp is inserted at every page bottom.
+html_last_updated_fmt = ''
---
+++ docs/src/index.rst
@@ -0,0 +1,28 @@
+Welcome to rasterio/affine's documentation!
+===========================================
+
+This is the documentation of the latest development version at https://github.com/rasterio/affine .
+
+.. include:: ../../README.rst
+
+Module documentation
+====================
+
+.. automodule:: affine
+   :members:
+
+.. include:: ../../CHANGES.txt
+.. include:: ../../AUTHORS.txt
+
+License
+=======
+
+.. include:: ../../LICENSE.txt
+   :literal:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
