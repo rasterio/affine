7dad52d3eeec7e20268dbb076e6cbe44e14f33ab
Mike Taves <mwtoews@gmail.com>
2024-12-19 13:02:15 +1300

    Update pre-commit versions and one more pyupgrade fix

--- affine/__init__.py
+++ affine/__init__.py
@@ -592,7 +592,7 @@ def loadsw(s: str):
         raise TypeError("Cannot split input string")
     coeffs = s.split()
     if len(coeffs) != 6:
-        raise ValueError("Expected 6 coefficients, found %d" % len(coeffs))
+        raise ValueError(f"Expected 6 coefficients, found {len(coeffs)}")
     a, d, b, e, c, f = (float(x) for x in coeffs)
     center = tuple.__new__(Affine, [a, b, c, d, e, f, 0.0, 0.0, 1.0])
     return center * Affine.translation(-0.5, -0.5)
