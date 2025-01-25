61c31c6a698baa2eeb1aa4f89aeb9bf450ccab16
Mike Taves <mwtoews@gmail.com>
2025-01-26 06:35:18 +1300

    Misc: more lints, tests and minor fixes (#119)

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -12,27 +12,29 @@ except ImportError:
 
 
 def test_array():
-    tfm = Affine(*np.linspace(0.1, 0.6, 6))
-    tfm_ar = np.array(tfm)
+    a, b, c, d, e, f = (np.arange(6) + 1) / 10
+    tfm = Affine(a, b, c, d, e, f)
     expected = np.array(
         [
-            [0.1, 0.2, 0.3],
-            [0.4, 0.5, 0.6],
-            [0.0, 0.0, 1.0],
+            [a, b, c],
+            [d, e, f],
+            [0, 0, 1],
         ],
     )
-    assert tfm_ar.shape == (3, 3)
-    assert tfm_ar.dtype == np.float64
-    testing.assert_allclose(tfm_ar, expected)
+    ar = np.array(tfm)
+    assert ar.shape == (3, 3)
+    assert ar.dtype == np.float64
+    testing.assert_array_equal(ar, expected)
 
     # dtype option
-    tfm_ar = np.array(tfm, dtype=np.float32)
-    assert tfm_ar.shape == (3, 3)
-    assert tfm_ar.dtype == np.float32
-    testing.assert_allclose(tfm_ar, expected)
+    ar = np.array(tfm, dtype=np.float32)
+    assert ar.shape == (3, 3)
+    assert ar.dtype == np.float32
+    testing.assert_allclose(ar, expected)
 
     # copy option
-    tfm_ar = np.array(tfm, copy=True)  # default None does the same
+    ar = np.array(tfm, copy=True)  # default None does the same
+    testing.assert_allclose(ar, expected)
 
     # Behaviour of copy=False is different between NumPy 1.x and 2.x
     if int(np.version.short_version.split(".", 1)[0]) >= 2:
@@ -40,3 +42,27 @@ def test_array():
             np.array(tfm, copy=False)
     else:
         testing.assert_allclose(np.array(tfm, copy=False), expected)
+
+
+def test_linalg():
+    # cross-check properties with numpy's linear algebra module
+    ar = np.array(
+        [
+            [0, -2, 2],
+            [3, 0, 5],
+            [0, 0, 1],
+        ]
+    )
+    tfm = Affine(*ar.flatten())
+    assert tfm.determinant == pytest.approx(6.0)
+    assert np.linalg.det(ar) == pytest.approx(6.0)
+
+    expected_inv = np.array(
+        [
+            [0, 1 / 3, -5 / 3],
+            [-1 / 2, 0, 1],
+            [0, 0, 1],
+        ]
+    )
+    testing.assert_allclose(~tfm, expected_inv)
+    testing.assert_allclose(np.linalg.inv(ar), expected_inv)
