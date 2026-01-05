8f353a2d676bfef00ee7ba8951e78979534b4f88
Rotzbua <Rotzbua@users.noreply.github.com>
2026-01-05 10:58:14 +0100

    chore(docs): upgrade sphinx
    use numpy  doc style
    fix minor warnings

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.abspath("../../src"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "rasterio/affine"
-copyright = "2025, Sean Gillies"
+project_copyright = "%Y, Sean Gillies"
 author = "Sean Gillies"
 release = "development"
 
@@ -21,28 +21,20 @@ release = "development"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",  # NumPy doc style.
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["Thumbs.db", ".DS_Store"]
-
-# The suffix of source filenames.
-source_suffix = ".rst"
-
-# The root toctree document.
-root_doc = "index"
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "nature"
-
 html_static_path = ["_static"]
 
 # If this is not None, a 'Last updated on:' timestamp is inserted at every page bottom.
